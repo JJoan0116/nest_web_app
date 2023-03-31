e98119d9949d72261a1af034b1039bfb0b78abae
JJoan <1720152558@qq.com>
2023-03-31 16:48:28 +0800

    feat: change test1 log

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('test1');
+  console.log('test1-1');
   await app.listen(3000);
   console.log('success');
 }
